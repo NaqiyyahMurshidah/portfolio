f9b56a64dab778556c90c68e9feb1e10275c327a
NaqiyyahMurshidah <naqiyyahmurshidah1@gmail.com>
2025-07-18 00:14:44 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  './path/to/your/style.css'],
+  content: ["./index.html", "./src/**/*.{js,css}"],
   theme: {
     extend: {
       colors: {
         "neon-white": "#FAFAFA", // or any other shade you prefer
+      },
+      keyframes: {
+        moveRight: {
+          "0%": { transform: "translateX(-10%)" },
+          "100%": { transform: "translateX(10%)" },
+        },
+        moveLeft: {
+          "0%": { transform: "translateX(10%)" },
+          "100%": { transform: "translateX(-10%)" },
+        },
+      },
+      animation: {
+        moveRight: "moveRight 5s ease-in-out infinite alternate",
+        moveLeft: "moveLeft 5s ease-in-out infinite alternate",
       },
     },
   },
